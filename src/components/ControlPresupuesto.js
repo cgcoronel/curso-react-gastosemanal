eea59f1d1cc3771707fcf6ac7849fa434eb6c537
Cristian Coronel <cgc474@hotmail.com>
2018-09-09 01:02:41 -0300

    1st commmit

---
+++ src/components/ControlPresupuesto.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Presupuesto from './Presupuesto';
+import Restante from './Restante';
+
+class ControlPresupuesto extends React.Component {
+
+	render(){
+		return (
+			<React.Fragment>
+				<Presupuesto
+						presupuesto={this.props.presupuesto}
+					/>
+				<Restante
+						presupuesto={this.props.presupuesto}
+						restante={this.props.restante}
+					/>
+			</React.Fragment>
+		)
+	}
+
+}
+
+export default ControlPresupuesto;
---
+++ src/components/Restante.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { revisarPresupuesto } from '../helper';
+
+class Restante extends React.Component {
+
+	render(){
+		const presupuesto = this.props.presupuesto;
+		const restante = this.props.restante;
+		return (
+			<div className={
+					revisarPresupuesto(presupuesto, restante)
+				}>
+				Restante: $ {this.props.restante}.-
+			</div>
+		)
+	}
+}
+export default Restante;
